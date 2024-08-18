646b88132d706910a2021cfd6d0eb0a50e575190
Arup8 <marup1784@gmail.com>
2024-08-18 14:56:48 +0530

    pic upload

--- src/components/Department/Department.jsx
+++ src/components/Department/Department.jsx
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react';
 import './Department.css';
 import photo1 from '../../img/dept01.JPG';
 import photo2 from '../../img/dept02.JPG';
-import photo3 from '../../img/dept03.JPG';
+import photo3 from '../../img/dept03.jpeg';
 import photo4 from '../../img/dept04.jpeg';
 import photo5 from '../../img/dept05.jpeg';
 import photo6 from '../../img/dept06.jpeg';
-import photo7 from '../../img/dept07.JPG';
-import photo8 from '../../img/dept08.JPG';
-import photo9 from '../../img/dept09.JPG';
+import photo7 from '../../img/dept07.jpeg';
+import photo8 from '../../img/dept08.jpeg';
+import photo9 from '../../img/dept09.jpeg';
+import photo10 from '../../img/dept10.jpeg';
+import photo11 from '../../img/dept11.jpeg';
+import photo12 from '../../img/dept12.jpeg';
 
-const photos = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9];
+const photos = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9, photo10, photo11, photo12];
 
 const Department = () => {
   const [activeIndex, setActiveIndex] = useState(0);
